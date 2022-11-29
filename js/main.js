0115139bcc14901af96aab5d1d522aefbd55d6a0
DiabloDraco <mirmuhsinmirahmatov@gmail.com>
2022-11-29 23:22:17 +0500

    added slider

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ let bg = document.getElementById('shadow');
 
 
 
-burger.addEventListener('click', function(){
+burger.addEventListener('click', function () {
     if (menu.classList.contains('active')) {
         menu.classList.remove('active');
         bg.classList.remove('active');
@@ -18,7 +18,7 @@ burger.addEventListener('click', function(){
     }
 });
 
-cl.addEventListener('click', function(){
+cl.addEventListener('click', function () {
     if (menu.classList.contains('active')) {
         menu.classList.remove('active');
         bg.classList.remove('active');
@@ -28,4 +28,33 @@ cl.addEventListener('click', function(){
     }
 });
 
+let swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 66,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper__top-next",
+        prevEl: ".swiper__top-prev"
+    }
+});
 
+if (window.outerWidth < 900) {
+    document.querySelector('.swiper-pagination').remove()
+    swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        spaceBetween: 1,
+        slidesPerGroup: 1,
+        loop: true,
+        loopFillGroupWithBlank: false,
+        navigation: {
+            nextEl: ".swiper__top-next",
+            prevEl: ".swiper__top-prev"
+        }
+    });
+}
