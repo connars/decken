a30859446c028a3502660f060bc13cdfffecf07c
Maks Skomorokha <91227382+connars@users.noreply.github.com>
2022-12-03 03:09:03 +0200

    add

--- js/main.js
+++ js/main.js
@@ -8,11 +8,11 @@ let bg = document.getElementById('shadow');
 
 burger.addEventListener('click', function () {
     if (menu.classList.contains('active')) {
-        menu.style.display = "none"
+        // menu.style.display = "none"
         menu.classList.remove('active');
         bg.classList.remove('active');
     } else {
-        menu.style.display = "flex"
+        // menu.style.display = "flex";
         menu.classList.toggle('active');
         bg.classList.toggle('active');
     }
@@ -20,7 +20,7 @@ burger.addEventListener('click', function () {
 
 cl.addEventListener('click', function () {
     if (menu.classList.contains('active')) {
-        menu.style.display = "none"
+        // menu.style.display = "none"
         menu.classList.remove('active');
         bg.classList.remove('active');
     } else {
